b04ffc117907232c44a79dfc87eb43273701dba1
Afzal-Shariff <afzalshariff0302@gmail.com>
2017-06-04 22:47:59 +0530

    Commited sqltestworking code

---
+++ MySQLTest/src/main/java/com/connectectivity/connect/MySqlCon.java
@@ -0,0 +1,52 @@
+package com.connectectivity.connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MySqlCon {
+
+	public static void main(String args[])  {
+
+		try{
+			Class.forName("com.mysql.jdbc.Driver"); 
+		} catch (Exception e) {
+			System.out.println("Error While connecting to Driver");
+			e.printStackTrace();
+		}
+
+		Connection con = null;
+		try {
+			String url ="jdbc:mysql://myrdsinstance.c4vfq4x8xx4n.us-west-2.rds.amazonaws.com:3306/myrdsinstance";
+			String username = "myrdsinstance";
+			String password = "myrdsinstance";
+			con = DriverManager.getConnection(url,username,password);
+
+		} catch(SQLException e){
+			System.out.println("Connection Failed! Please Check console");
+			e.printStackTrace();
+		}
+
+		if(con != null) {
+			try{
+				Statement stm = con.createStatement();
+				String sql = "select * from user";
+				ResultSet rs = stm.executeQuery(sql);
+				while(rs.next()){
+					String user = rs.getString(1);
+					String mail = rs.getString(2);
+					String pswrd = rs.getString(3);
+					String curTime = rs.getString(4);
+					
+					System.out.println(user+"   " + mail +"   " + pswrd+ "   " + curTime);
+				} 
+				con.close();
+			}
+
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}}
